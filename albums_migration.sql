28411f6f0d5119f36e2a7b3ac933f6d5591548ad
Brad Colton <bradcolton11@yahoo.com>
2016-12-07 09:50:36 -0600

    changed to mimic example

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,3 +1,3 @@
 use codeup_test_db;
 drop table if exists albums;
-create table albums(id int unsigned not null auto_increment, artist varchar(50) not null, name varchar(50) not null, release_date year not null, sales float not null, genre varchar(20) not null, primary key(id));
+create table albums(id int unsigned not null auto_increment, artist varchar(100) not null, album varchar(100) not null, genre varchar(100) not null, release_date year not null, sales_in_millions decimal(8,2) not null, primary key(id));
