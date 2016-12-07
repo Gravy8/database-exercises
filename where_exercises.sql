c3480c12929cec79c3a972724dc25c1ffe63eb70
Brad Colton <bradcolton11@yahoo.com>
2016-12-07 16:26:45 -0600

    completed next exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,8 +1,8 @@
 USE employees;
 
 
-select * FROM employees WHERE first_name IN ('irena', 'vidya', 'maya');
-select * FROM employees WHERE last_name LIKE 'e%';
-select * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
-SELECT * FROM employees WHERE birth_date LIKE '%-12-25';
-SELECT * FROM employees WHERE last_name LIKE '%q%';
+select * FROM employees WHERE (first_name = 'irena' OR first_name = 'vidya' OR first_name = 'maya') AND gender = 'm';
+select * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
+SELECT * FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e';
+select * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%-12-25';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
